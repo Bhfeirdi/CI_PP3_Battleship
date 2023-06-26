5c70b707842e90d9a8fb90ef3dc2f0d86bf50650
Bhfeirdi <ferdia.link@gmail.com>
2023-06-26 03:26:07 +0100

    main close

--- run.py
+++ run.py
@@ -148,3 +148,6 @@ def main():
             play_again = input("Do you want to play again? (yes/no): ")
             if play_again.lower() != "yes":
                 break
+
+if __name__ == "__main__":
+    main()
